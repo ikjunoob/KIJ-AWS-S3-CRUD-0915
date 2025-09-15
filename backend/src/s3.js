af3f35e0f69448a749ebe744e744e1ea6f231ead
KIJ <dlrwn0730@naver.com>
2025-09-15 20:50:29 +0900

    .

--- backend/src/s3.js
+++ backend/src/s3.js
@@ -1,5 +1,11 @@
-import { S3Client, PutObjectCommand, GetObjectCommand, DeleteObjectCommand } from "@aws-sdk/client-s3";
+import {
+    S3Client,
+    PutObjectCommand,
+    GetObjectCommand,
+    DeleteObjectCommand
+} from "@aws-sdk/client-s3";
 import { getSignedUrl } from "@aws-sdk/s3-request-presigner";
+
 
 export const s3 = new S3Client({
     region: process.env.AWS_REGION
@@ -7,12 +13,14 @@ export const s3 = new S3Client({
 
 const Bucket = process.env.S3_BUCKET
 
+
 export const presignPut = (Key, ContentType) =>
     getSignedUrl(
         s3,
         new PutObjectCommand({ Bucket, Key, ContentType }),
         { expiresIn: 300 }
     )
+
 
 export const presignGet = (Key, sec = 300) =>
     getSignedUrl(
